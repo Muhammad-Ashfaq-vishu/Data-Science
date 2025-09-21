35731e0b84b817e2ca17296778ebcc0703cce84e
ashfaqvishu <ashfaqvishu@gmail.com>
2025-09-21 07:37:56 +0500

    Updated code

---
+++ python (1)/Day 1/inputQS.py
@@ -0,0 +1,79 @@
+
+
+# Q no 11 Take your name as input and print "Hello, <name>".
+
+name = input("Enter your name") 
+print("Your name is", name)
+
+
+
+
+# Q no 12 Take two numbers as input and print their sum.
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+total = num1 + num2
+
+print("The sum is:", total)
+
+
+
+# # Q no 13 Take a number as input and print its square.
+
+num = int(input("Enter a number: "))
+square = num * num
+print("The square of the number is:", square)
+
+
+
+# Q no 14 Input your age and print "You will be <age+5> years old after 5 years.
+
+age = int(input("Enter your age: "))
+future_age = age + 5
+print("You will be", future_age, "years old after 5 years.")
+
+
+
+
+# Q no 15 Take two inputs: first name and last name, then print the full name.
+
+first_name = input("Enter your first name :")
+last_name = input("Enter your last name :")
+Full_name = first_name + " " + last_name
+print("Your full name is :", Full_name)
+
+
+
+# Q no 16 Take two numbers as input and print their addition, subtraction, multiplication, and division.
+
+num1 = int(input("Enter your first number :"))
+num2 = int(input("Enter your second number :"))
+
+addition = num1 + num2
+subtraction = num1 - num2   
+multiplication = num1 * num2
+division = num1 / num2
+print("The addition is :", addition)
+print("The subtraction is :", subtraction)
+print("The multiplication is :", multiplication)
+print("The division is :", division)
+
+
+
+# Q no 17 Print the remainder when one number is divided by another.
+num1 = int(input("Enter your first number :"))
+num2 = int(input("Enter your second number :"))
+remainder = num1 % num2
+print("The remainder is :", remainder)
+
+
+# 18. Print the square and cube of a number using the ** operator
+
+num = int(input("Enter a number :"))
+square = num ** 2
+cube = num ** 3
+print("The square is :", square)
+print("The cube is :", cube)
+
+
